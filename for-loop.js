36bc713d25d9a4e58d885088519f5fd8f7457988
shantoe <shantoroy1792@gmail.com>
2022-03-04 00:35:07 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,6 @@
+
+var morArray = ['number0', 'index1', 'index2', 'array3', 'moreArray'];
+for(var i = 0; i < morArray.length; i++){
+  var showArrayValue = morArray[i];
+  console.log(showArrayValue);
+}
---
+++ while-loop.js
@@ -0,0 +1,10 @@
+// firest delacer variable
+// then condition
+// then console.log
+// then increment/dicrement
+var x= 4;
+while(x < 10){
+  console.log(x);
+  x++
+}
+
